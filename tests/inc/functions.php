70d2a71f25e359ebf9c632bb07e20e58a929aa0b
Igor Tron <public@itron.pro>
2024-08-27 17:05:03 +0400

    remove_spaces_recursively()

--- tests/inc/functions.php
+++ tests/inc/functions.php
@@ -6,3 +6,11 @@
 function remove_spaces( string $string ): string {
 	return preg_replace( '/\s+/', '', $string );
 }
+
+function remove_spaces_recursively( array $array ): array {
+	array_walk_recursive( $array, function( &$value ) {
+		$value = remove_spaces( $value );
+	} );
+
+	return $array;
+}
