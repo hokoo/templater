06336b02734ca2a166fe52f2700b3eb5fdef3151
Igor Tron <public@itron.pro>
2024-09-16 01:54:40 +0400

    Templater class

---
+++ src/Templater.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace iTRON\Anatomy;
+
+class Templater {
+
+	public function render(
+		string $template,
+		array $data
+	) {}
+
+	public function renderBlock(
+		string $template,
+		string $repeater,
+		array $data
+	) {}
+}
