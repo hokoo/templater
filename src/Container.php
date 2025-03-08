bd454984e44cf45c0ab233f47b0861c9fafa08ae
Igor Tron <public@itron.pro>
2025-03-08 20:06:10 +0400

    Container

--- src/Container.php
+++ src/Container.php
@@ -5,6 +5,8 @@ namespace iTRON\Anatomy;
 use ArrayObject;
 
 class Container extends ArrayObject {
+	protected Core $context;
+
 	public function addText( string $text ): static {
 		$this->append( $this->getElementSchema( data: $text ) );
 
@@ -12,12 +14,12 @@ class Container extends ArrayObject {
 	}
 
 	public function addBlock( string $name, array $data ): static {
-		array_walk(
-			$data,
-			function ( &$value ) {
-				$value = is_a( $value, static::class ) ? $value->getArrayCopy() : $value;
-			}
-		);
+//		array_walk(
+//			$data,
+//			function ( &$value ) {
+//				$value = is_a( $value, static::class ) ? $value->getArrayCopy() : $value;
+//			}
+//		);
 
 		$this->append( $this->getElementSchema( $name, $data ) );
 
@@ -31,10 +33,44 @@ class Container extends ArrayObject {
 			$schema[ Core::BLOCK_NAME_SCHEMA_KEY ] = $blockName;
 		}
 
-		if ( ! empty( $data ) ) {
-			$schema[ Core::DATA_SCHEMA_KEY ] = $data;
-		}
+		$schema[ Core::DATA_SCHEMA_KEY ] = $data;
 
 		return $schema;
+	}
+
+	public function getContext(): Core {
+		return $this->context;
+	}
+
+	public function setContext( Core $context ): static {
+		$this->context = $context;
+
+		return $this;
+	}
+
+	public function __toString(): string {
+		if ( empty( $this->getContext() ) ) {
+			return '';
+		}
+
+		return $this->context->renderContainer( $this );
+	}
+
+	/**
+	 * Convert the object to its array representation recursively.
+	 */
+	public function getArrayCopy(): array {
+		$result = parent::getArrayCopy();
+
+		foreach ( $result as $key => $value ) {
+			if ( is_array( $value[ Core::DATA_SCHEMA_KEY ] ) )
+			foreach ( $value[ Core::DATA_SCHEMA_KEY ] as $k => $v ) {
+				if ( $v instanceof static ) {
+					$result[ $key ][ Core::DATA_SCHEMA_KEY ][ $k ] = $v->getArrayCopy();
+				}
+			}
+		}
+
+		return $result;
 	}
 }
