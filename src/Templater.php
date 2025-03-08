1c05d6bbe5cc49e907d3b3a74bdb387391fa8ead
Igor Tron <public@itron.pro>
2025-03-08 20:06:18 +0400

    Templater

--- src/Templater.php
+++ src/Templater.php
@@ -7,11 +7,45 @@ class Templater {
 	public function render(
 		string $template,
 		array $data
-	) {}
+	): string {
+		$engine = new Core(
+			$template,
+			$data
+		);
+
+		$this->defineContext( $data, $engine );
+
+		return $engine->render();
+	}
 
 	public function renderBlock(
 		string $template,
-		string $repeater,
+		string $blockName,
 		array $data
-	) {}
+	): string {
+		$engine = new Core(
+			$template,
+			$data
+		);
+
+		$this->defineContext( $data, $engine );
+
+		return $engine->render( $blockName );
+	}
+
+	protected function defineContext( array $data, Core $context ): static {
+		foreach ( $data as $key => $value ) {
+			if ( $value instanceof Container ) {
+				$value->setContext( $context );
+				$inner = (array) $value;
+
+				$set = array_column( $inner, Core::DATA_SCHEMA_KEY );
+				// Filter out empty or non-array values
+				$set = array_filter( $set, 'is_array' );
+				$this->defineContext( array_merge( ...$set ), $context );
+			}
+		}
+
+		return $this;
+	}
 }
